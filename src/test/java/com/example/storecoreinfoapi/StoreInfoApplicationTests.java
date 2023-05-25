ceb465e5ae864a7a812f468f058b1ba204f31bb3
Anthony Ikeda <anthony.ikeda@gmail.com>
2023-05-25 15:27:38 -0700

    Removing contextLoads test for now

--- src/test/java/com/example/storecoreinfoapi/StoreInfoApplicationTests.java
+++ src/test/java/com/example/storecoreinfoapi/StoreInfoApplicationTests.java
@@ -3,11 +3,7 @@ package com.example.storecoreinfoapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class StoreInfoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	
 }
